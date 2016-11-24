e1e5a30b67547401655ac0bd68b92659a61087b7
shmizan <jointdogg@gmail.com>
2016-11-24 19:04:43 +0200

    updated 1 funct

--- ex2/my_grep.c
+++ ex2/my_grep.c
@@ -4,38 +4,81 @@
 #include "helper.h"
 
 
+#define TRUE 1
+#define FALSE 0
 #define ERROR 1
 #define OK 0
 
 typedef struct grep_options_struct
 {
 	int value_for_a;
-	//bool is_b_active;
-	//bool is_c_active;
-	//bool is_i_active;
-	//bool is_n_active;
-	//bool is_v_active;
-	//bool is_x_active;
+	int is_b_active;
+	int is_c_active;
+	int is_i_active;
+	int is_n_active;
+	int is_v_active;
+	int is_x_active;
 	int argv_index_for_target_str;
 	int argv_index_for_file_name;
 } grep_options_struct;
 
-void get_grep_options(grep_options_struct *grep_options,char *argv[])
-{
-	//parsing of string to search and file name will be here
 
-	grep_options->argv_index_for_target_str=1;
-	grep_options->argv_index_for_file_name=2;
+void init_grep_options(grep_options_struct *grep_options)
+{
+	
 }
 
-FILE* get_file(char *argv[], grep_options_struct grep_options)
+void get_grep_options(grep_options_struct *grep_options,char *argv[],int argc)
+{
+	int argc_index, search_term_found=FALSE, target_file_found=FALSE;
+	grep_options->argv_index_for_file_name=-1;
+
+	for (argc_index=1;argc_index<argc;argc_index++)
+	{
+		printf("%s",argv[argc_index]);
+
+		if (!strcmp(argv[argc_index],"-b")) {
+			grep_options->is_b_active=TRUE;
+			continue;
+		}
+		if (!strcmp(argv[argc_index],"-c")) {
+			grep_options->is_c_active=TRUE;
+			continue;
+		}
+		if (!strcmp(argv[argc_index],"-i")) {
+			grep_options->is_i_active=TRUE;
+			continue;
+		}
+		if (!strcmp(argv[argc_index],"-n")) {
+			grep_options->is_n_active=TRUE;
+			continue;
+		}
+		if (!strcmp(argv[argc_index],"-v")) {
+			grep_options->is_v_active=TRUE;
+			continue;
+		}
+		if (!strcmp(argv[argc_index],"-x")) {
+			grep_options->is_x_active=TRUE;
+			continue;
+		}
+		if (search_term_found==FALSE) {
+			grep_options->argv_index_for_target_str=argc_index;
+			search_term_found=TRUE;
+			continue;
+		}
+		if (target_file_found==FALSE) {
+			grep_options->argv_index_for_file_name=argc_index;
+			target_file_found=TRUE;
+			continue;
+		}
+	}
+}
+
+FILE *get_file(char *argv[], grep_options_struct grep_options)
 {
 	FILE *fp;
-
-	if (grep_options.argv_index_for_file_name==-1)
-		return stdin; 
 	fp=fopen(argv[grep_options.argv_index_for_file_name],"r");
-	if(fp==NULL)
+	if (fp==NULL)
 		exit(ERROR);
 	return fp;
 }
@@ -44,25 +87,31 @@ FILE* get_file(char *argv[], grep_options_struct grep_options)
 int main(int argc, char *argv[])
 {
 	FILE *file;
+	char parser;
+	int line_size;
 
 	char **line=NULL;
 	size_t n;
 	char *pointer_to_searched_substring;
+	int index_for_file_name_in_argv;
+
+	//here is sub stringggggggggggggggggggggggggggg
+	char sub_string[]="bla";
+
+	char *file_name;
 	grep_options_struct grep_options;
 
-
-	
-	get_grep_options(&grep_options,argv);
+	get_grep_options(&grep_options,argv,argc);
 	file=get_file(argv,grep_options);
-
-
 	//get_file_name(file_name,argv,index_for_file_name_in_argv);
 	//=argv[2];
 
 	//file=fopen(file_name,"r");
 
+	if (file==NULL)
+		return ERROR;
+
 	getline(line,&n,file);
-	printf("%s",line);
 	while(!feof(file))
 	{
 
@@ -70,9 +119,9 @@ int main(int argc, char *argv[])
 
 
 		//print if found
-		pointer_to_searched_substring=strstr(*line,argv[grep_options.argv_index_for_target_str]);
-		//if (!pointer_to_searched_substring==NULL)
-			//printf("%s", *line);//will be inside a function so ok
+		pointer_to_searched_substring=strstr(*line,sub_string);
+		if (!pointer_to_searched_substring==NULL)
+			printf("%s", *line);//will be inside a function so ok
 		getline(line,&n,file);
 	}
 
@@ -81,7 +130,5 @@ int main(int argc, char *argv[])
 	return OK;
 
 
+
 }
-
-
-
