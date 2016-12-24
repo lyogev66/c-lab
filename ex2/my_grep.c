c2815d29e8c98b391f469d3f5125c75378278b8d
yogev laks <lyogev66@users.noreply.github.com>
2016-12-24 14:53:12 +0200

    extracted all the "main part" of the script from the int main

--- ex2/my_grep.c
+++ ex2/my_grep.c
@@ -3,23 +3,14 @@
 #include "my_grep.h"
 #include "my_grep_functions.h"
 
-int main(int argc, char *argv[])
+void run_my_grep_routine(FILE *file,grep_options_struct grep_options)
 {
-	FILE *file=NULL;
-	line_descriptor_struct line_descriptor;
-	grep_options_struct grep_options;
 	char *line=NULL;
+	line_descriptor_struct line_descriptor;
 
-	if (argc<MIN_ARGUMENT_NUMBER){
-		printf("NOT enough arguments\n"); 
-		exit(0);
-	}
-	get_grep_arguments(&grep_options,argv,argc);
-	file=open_file_or_stdin(grep_options);
 	init_line_descriptor_struct(&line_descriptor);
 	read_line(file,&line,&line_descriptor);
 	while(!feof(file)){
-
 		fill_line_descriptor(&line_descriptor,line,grep_options);
 		if (line_descriptor.number_of_lines_remained_to_print>0){
 			print_match(line_descriptor,grep_options);
@@ -33,5 +24,22 @@ int main(int argc, char *argv[])
 	free(line);
 	free(grep_options.regex_array);
 	fclose(file);
+}
+
+
+int main(int argc, char *argv[])
+{
+	FILE *file=NULL;
+	grep_options_struct grep_options;
+
+
+	if (argc<MIN_ARGUMENT_NUMBER){
+		printf("NOT enough arguments\n"); 
+		exit(0);
+	}
+	get_grep_arguments(&grep_options,argv,argc);
+	file=open_file_or_stdin(grep_options);
+	run_my_grep_routine(file,grep_options);
 	return(0);
 }
+
